8cda6f0769edfd313186c971f20fd00a84a81f1c
alvinfaruq <alvinasshegaff77@gmail.com>
2020-09-17 20:44:09 +0700

    first commit

---
+++ Biodata Alvin Faruq.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\t >>> Biodata <<<\n");
+	printf("Nama		: Moch. Alvin Faruq Asshegaff\n");
+	printf("NPM		: 19081010131\n");
+	printf("Program Studi	: Teknik Informatika\n");
+	printf("Angkatan 	: 2019\n");
+	printf("TTL		: Surabaya, 06 Februari 2000\n");
+	printf("Alamat		: Wonoayu 8/62 RT002 RW003 Kel. Medokan Ayu Kec. Rungkut Kota Surabaya\n");
+return 0;
+}
